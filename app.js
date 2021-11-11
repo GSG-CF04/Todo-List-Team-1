d46fcfee448e8acf4cb97440c15c0d87ad471c9e
zaher-aa <zaherabuamro@gmail.com>
2021-11-11 09:50:38 +0200

    Edited Add & Edit Btns

--- app.js
+++ app.js
@@ -1,71 +1,33 @@
-
-let inputFiled=document.getElementById("input-task");
-let  addBtn=document.getElementById('add-task')
-let arrayItem=[];
-let specialId = new Date().getTime();
-function addTask(){
-let tasks={
-    val: inputFiled.value,
-    id: `${specialId}`, 
-    done:false
-}
-arrayItem.push(tasks)
-localStorage.setItem("tasks",JSON.stringify(tasks))
-}
-
-function displayTask(){
-let warpperTask=document.getElementById('tasks-wrapper')
-let divTask=document.createElement("div")
-divTask.setAttribute('class','task')
-divTask.setAttribute("data-id", specialId);
-let content=document.createElement("span")
-content.setAttribute('class','content')
-divTask.appendChild(content)
-let editDelete=createElement('div')
-editDelete.setAttribute('class','button-parent')
-let btnDelete=document.createElement('button')
-btnDelete.setAttribute('id','delete')
-editDelete.appendChild(btnDelete)
-let btnEdit=document.createElement('button')
-btnEdit.setAttribute('id','edit')
-editDelete.appendChild(btnEdit)
-divTask.appendChild(editDelete)
-warpperTask.prependChild(divTask)
-}
-addBtn.addEventListener('click', addTask)
-addBtn.addEventListener('click', displayTask)
-inputFiled.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-     addTask()
-     displayTask()
-    }
-});
-
-
-// Delete Task.
+// Delete task
 function delTask(e) {
-  if (!e.target.matches("#delete")) return;
+  if (!e.target.matches(".far.fa-trash-alt") && !e.target.matches("#delete"))
+    return;
   // Remove element from lacalStorage
   let allTasks = JSON.parse(localStorage.tasks);
-  let taskIdx = allTasks.findIndex(
-    (t) => t.id == e.target.parentElement.parentElement.dataset.id
-  );
+  let taskIdx = allTasks.findIndex((t) => {
+    return e.target.matches("#delete")
+      ? t.id == e.target.parentElement.parentElement.dataset.id
+      : t.id == e.target.parentElement.parentElement.parentElement.dataset.id;
+  });
   allTasks.splice(taskIdx, 1);
   localStorage.tasks = JSON.stringify(allTasks);
   // Remove task from page
-  e.target.parentElement.remove();
+  e.target.matches("#delete")
+    ? e.target.parentElement.parentElement.remove()
+    : e.target.parentElement.parentElement.parentElement.remove();
 }
 
 // Edit Task.
 let n = 2;
 function editTask(e) {
-  if (!e.target.matches("#edit")) return;
+  if (!e.target.matches(".far.fa-edit") && !e.target.matches("#edit")) return;
   // Edit content of task in the page
   let content = [...document.querySelectorAll(".content")].filter((t) => {
-    return (
-      t.parentElement.dataset.id ===
-      e.target.parentElement.parentElement.dataset.id
-    );
+    return e.target.matches("#edit")
+      ? t.parentElement.dataset.id ===
+          e.target.parentElement.parentElement.dataset.id
+      : t.parentElement.dataset.id ===
+          e.target.parentElement.parentElement.parentElement.dataset.id;
   })[0];
   if (n % 2 === 0) {
     content.setAttribute("contenteditable", "true");
@@ -74,9 +36,11 @@ function editTask(e) {
     content.removeAttribute("contenteditable");
     // Edit content of current element in localStorage.
     let allTasks = JSON.parse(localStorage.tasks);
-    let taskIdx = allTasks.findIndex(
-      (t) => t.id == e.target.parentElement.parentElement.dataset.id
-    );
+    let taskIdx = allTasks.findIndex((t) => {
+      return e.target.matches("#edit")
+        ? t.id == e.target.parentElement.parentElement.dataset.id
+        : t.id == e.target.parentElement.parentElement.parentElement.dataset.id;
+    });
     allTasks[taskIdx].val = content.textContent;
     localStorage.tasks = JSON.stringify(allTasks);
   }
@@ -88,6 +52,8 @@ document.addEventListener("click", (e) => {
   delTask(e);
   editTask(e);
 });
+
+// Dark Mode Toggle
 let toggleBtn = document.querySelector(".toggle-btn");
 let bodyElement = document.querySelector("body");
 
